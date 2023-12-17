594d7a512331edc071116e2f9c5c6e34354117e6
jaskrrish <jashehz@gmail.com>
2023-12-17 15:47:24 +0530

    BlogApp First Commit

---
+++ src/components/index.js
@@ -0,0 +1,31 @@
+import Header from "./Header/Header";
+import Footer from "./Footer/Footer";
+import Container from "./container/Container";
+import Logo from "./Logo";
+import Button from "./Button";
+import Input from "./Input";
+import Login from "./Login";
+import Select from "./Select";
+import Signup from "./Signup";
+import PostCard from "./PostCard";
+import PostForm from "./post-form/PostForm";
+import LogoutBtn from "./Header/LogoutBtn";
+import RTE from "./RTE";
+import AuthLayout from "./AuthLayout";
+
+export {
+  Header,
+  Footer,
+  Button,
+  Input,
+  Container,
+  Logo,
+  LogoutBtn,
+  RTE,
+  Login,
+  Select,
+  Signup,
+  PostCard,
+  PostForm,
+  AuthLayout,
+};
